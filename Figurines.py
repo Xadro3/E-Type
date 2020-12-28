eba7bbbe30d70bcea9681425089bcbfc6bcb6b52
Fabio <fmaier67@gmail.com>
2020-12-28 17:45:04 +0100

    commited

--- Figurines.py
+++ Figurines.py
@@ -4,13 +4,13 @@ import pygame
 def loadImage(filename, colorkey=None):
     image = pygame.image.load(filename)
 
-    if image.get_alpha() is None:
+    if image.get_alpha() == None:
         image = image.convert()
     else:
         image = image.convert_alpha()
 
-    if colorkey is not None:
-        if colorkey is -1:
+    if colorkey != None:
+        if colorkey == -1:
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey, pygame.RLEACCEL)
 
